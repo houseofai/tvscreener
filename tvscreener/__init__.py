4610d0a0fa4083a6a481e4d3f29b682f5cbeb323
Odyssée <8287111+houseofai@users.noreply.github.com>
2025-06-18 18:12:05 +0200

    Fix imports

--- tvscreener/__init__.py
+++ tvscreener/__init__.py
@@ -1,8 +1,12 @@
-# tvscreener/__init__.py
+from .core.base import Screener, ScreenerDataFrame
 from .core.stock import StockScreener
 from .core.forex import ForexScreener
 from .core.crypto import CryptoScreener
-from .core.base import Screener, ScreenerDataFrame
+
+__all__ = [
+    "Screener", "ScreenerDataFrame",
+    "StockScreener", "ForexScreener", "CryptoScreener",
+]
 from .field import *
 from .filter import *
 from .util import *
